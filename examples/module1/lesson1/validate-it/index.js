6c91eb8f34ab6e055bdeb48f66febda4a4c9d744
sid2602 <sidhd2602@gmail.com>
2024-06-20 17:03:34 +0200

    refactor: validate-it

--- examples/module1/lesson1/validate-it/index.js
+++ examples/module1/lesson1/validate-it/index.js
@@ -1,34 +1,57 @@
-function validator() {
+const ERROR_MESSAGE = 'Invalid';
+const SUCCESS_MESSAGE = 'Valid';
+
+const minAmountValidator = (minValue) => (value) => value > minValue;
+const maxAmountValidator = (maxValue) => (value) => value < maxValue;
+const moduloValidator = (moduloValue, equals) => (value) =>
+  value % moduloValue == equals;
+
+const validateInput = (inputValue, ...validators) => {
+  if (inputValue === '') {
+    return ERROR_MESSAGE;
+  }
+
+  const value = Number(inputValue);
+
+  if (!Number.isInteger(value)) {
+    return ERROR_MESSAGE;
+  }
+
+  if (validators.some((validator) => validator(value) === false)) {
+    return ERROR_MESSAGE;
+  }
+
+  return SUCCESS_MESSAGE;
+};
+
+const getElements = () => {
   const input = document.getElementById('input');
   const button = document.getElementById('button');
   const button2 = document.getElementById('button2');
   const result = document.getElementById('result');
 
-  button.addEventListener('click', () => {
-    if (input.value) {
-      if (Number.isInteger(input.value)) {
-        if (
-          Number(input.value) > 0 &&
-          Number(input.value) < 100 &&
-          Number(input.value) % 2 === 0
-        ) {
-          result.innerHTML = 'Valid';
-        } else {
-          result.innerHTML = 'Invalid';
-        }
-        result.innerHTML = 'Valid';
-      } else {
-        result.innerHTML = 'Invalid';
-      }
-    } else {
-      result.innerHTML = 'Invalid';
-    }
-  });
+  return { input, button, button2, result };
+};
 
-  button2.addEventListener('click', () => {
-    input.value = '';
-    result.innerHTML = '';
-  });
+const handleClear = (input, result) => {
+  input.value = '';
+  result.innerHTML = '';
+};
+
+const handleValidate = (input, result) => {
+  result.innerHTML = validateInput(
+    input.value,
+    minAmountValidator(0),
+    maxAmountValidator(100),
+    moduloValidator(2, 0)
+  );
+};
+
+function validator() {
+  const { input, button, button2, result } = getElements();
+
+  button.addEventListener('click', () => handleValidate(input, result));
+  button2.addEventListener('click', () => handleClear(input, result));
 }
 
 validator();
